2804d3e6b7bdd69f9e42a0e23badb7153ad6d615
Jessica Duell <duellsoftwaredev@gmail.com>
2026-01-05 18:44:16 -0500

    empty to give review on all files

--- client/src/api/questions.js
+++
@@ -1,9 +0,0 @@
-export async function getRandomQuestion() {
-  const res = await fetch("http://localhost:5000/questions/random");
-
-  if (!res.ok) {
-    throw new Error("Failed to fetch question");
-  }
-
-  return res.json();
-}
