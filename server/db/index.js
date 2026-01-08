7cc9b0170f106a5979570cd9fe7b53a3319279d5
Scott Fairdosi <scottfairdosi@yahoo.com>
2026-01-07 20:43:27 -0600

    Fix pg import and db config

--- server/db/index.js
+++ server/db/index.js
@@ -1,16 +1,16 @@
-import pkg from 'pg';
-import dotenv from 'dotenv';
+import pg from "pg";
+import dotenv from "dotenv";
 
 dotenv.config();
 
-const { Pool } = pkg;
+const { Pool } = pg;
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME
+  connectionString: process.env.DATABASE_URL,
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : false,
 });
 
 export default pool;
